3412a5ab59dbf0c50b3bc1f0a4d943d1b97b8b8b
Natan-TI <natan201125@hotmail.com>
2026-02-11 22:08:11 -0300

    Config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+const repoName = "kana-flashcards";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
